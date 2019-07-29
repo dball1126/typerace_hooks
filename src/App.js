84fad30f05fca8d2f8c4240354c8d2774cb980fd
daniel <g.thirteen333@gmail.com>
2019-07-28 23:13:11 -0400

    snippets added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 
 
 const App = () => {
+  const SNIPPETS = ['Bears, beets, battlestar galactica', "What's Forrest Gump's password? 1Forrest1", 'Where do programmers like to hangout?  The Foo Bar'];
+  const [snippet, setSnippet] = useState('');
   const [userText, setUserText] = useState('');
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+  };
 
   const updateUserText = event => {
     setUserText(event.target.value);
@@ -14,7 +21,18 @@ const App = () => {
   return (
     <div>
       <h2>Type Race</h2>
+      <hr />
+      <h3>Snippet</h3>
+      {snippet}
       <input value={userText} onChange={updateUserText} />
+      <hr />
+      {
+        SNIPPETS.map((SNIPPET, index) => (
+          <button onClick={chooseSnippet(index)} key={index}>
+            {SNIPPET.substring(0, 10)}...
+          </button>
+        ))
+      }
     </div>
   )
 }
